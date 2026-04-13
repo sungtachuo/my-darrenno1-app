eaba3e78a16d9bbfafa81513f1ee2a6c089f6112
Darren Cho <darrencho@PhoebedeiMac.local>
2026-04-14 00:41:16 +0800

    fix: fix typo responseMimeType

--- netlify/functions/gemini-proxy.js
+++ netlify/functions/gemini-proxy.js
@@ -30,7 +30,7 @@ exports.handler = async (event) => {
           }
         ],
         generationConfig: {
-          responseMimeYype: "application/json", // 強制要求回傳純 JSON
+          responseMimeType: "application/json", // 強制要求回傳純 JSON
           temperature: 0.1 // 降低隨機性，確保結果精確
         }
       })
